bda46d138e117c5dc9d24a3b0069465b1f5224a0
Dhruv Atri <dhruvatri@Dhruvs-MacBook-Air.local>
2024-03-01 01:12:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db= new pg.Client({
+  user:"postgres",
+  password:"postgres",
+  database:"world",
+  port:5432,
+  host:"localhost"
+});
+
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+async function checkVisisted()
+{
+  const result = await db.query("SELECT country_code from visited_countries");
+  let countries=[];
+  result.rows.forEach((country)=>{
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries = await checkVisisted();
+  res.render("index.ejs", {countries: countries, total:countries.length});
+});
+
+app.post("/add",async (req,res)=>{
+  const input = req.body.country;
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name like '%' || $1 || '%';",
+      [input]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
